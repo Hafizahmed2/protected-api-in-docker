dd58987464a3ce7e5727c0e098d6e42f947f0951
Hafiz Ahmed <hafizahmed.tx@gmail.com>
2022-08-29 10:55:01 -0700

    corrected routes

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const timeRouter = require("./modules/time/routes");
 const authRouter = require("./modules/authentication/routes");
 
 app
+  .use("/auth", authRouter)
+  .use("/time", timeRouter)
   .use("/", (req, res) =>
     res.send("Welcome to Protected Api in a Docker Container")
-  )
-  .use("/auth", authRouter)
-  .use("/time", timeRouter);
+  );
 
 // Handle errors.
 app.use(function (err, req, res, next) {
